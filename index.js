f97d85606db92e10d313362ba841049d39c59be8
Folkert Niewijk <folkert.niewijk@marks-and-spencer.com>
2016-12-14 18:26:01 +0000

    Added watch capability
    Added watch capability. Every time something changes in the specified directory, sprity is run again. This is not a smart algorithm.
    
    Took a liberal inspiration from webpack-spritesmith.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var gaze = require('gaze');
+var glob = require('glob');
 var sprity = require('sprity');
 
 function SprityWebpackPlugin(options) {
@@ -8,6 +10,27 @@ SprityWebpackPlugin.prototype.apply = function(compiler) {
   compiler.plugin('run', function (compiler, callback) {
     sprity.create(this.options, function() {
       callback();
+    });
+  }.bind(this));
+
+  var watchStarted = false;
+  compiler.plugin('watch-run', function (watching, watchRunCallback) {
+    if (watchStarted) {
+      return watchRunCallback();
+    }
+    watchStarted = true;
+    gaze(
+      this.options.src,
+      {},
+      function (err, gaze) {
+        err && fThrow(err);
+        gaze.on('all', function () {
+          sprity.create(this.options, function() {});
+        }.bind(this));
+      }.bind(this)
+    );
+    return sprity.create(this.options, function() {
+      watchRunCallback();
     });
   }.bind(this));
 };
